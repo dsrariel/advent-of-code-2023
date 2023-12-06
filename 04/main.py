223fd050034c5840b9f79378dc7b7383bc875b5d
Ariel Ribeiro <dsrariel@gmail.com>
2023-12-06 16:44:22 -0300

    solve part 2

--- 04/main.py
+++ 04/main.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 FILE_NAME = "input.txt"
 
 
@@ -12,6 +14,27 @@ def get_card_matches(numbers: str) -> int:
             matches += 1
 
     return matches
+
+
+def part2():
+    total_cards = 0
+    card_to_copies_count = defaultdict(lambda: 0)
+    with open(FILE_NAME, encoding="utf-8") as f:
+        for line in f.readlines():
+            card_and_numbers = line.split(": ", 1)
+            card_number = int(card_and_numbers[0][len("Card ") :])
+            matches = get_card_matches(card_and_numbers[1])
+
+            copies_count = card_to_copies_count[card_number] + 1
+            del card_to_copies_count[card_number]
+
+            for i in range(matches):
+                card_won = card_number + i + 1
+                card_to_copies_count[card_won] += copies_count
+
+            total_cards += copies_count
+
+    print(f"The total cards you end up with is {total_cards}")
 
 
 def part1():
@@ -31,3 +54,4 @@ def part1():
 
 if __name__ == "__main__":
     part1()
+    part2()
