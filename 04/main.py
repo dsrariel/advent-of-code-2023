36f86939be31c8fbd93ada080898bf15b4832f64
Ariel Ribeiro <dsrariel@gmail.com>
2023-12-06 16:14:56 -0300

    extract function

--- 04/main.py
+++ 04/main.py
@@ -1,19 +1,26 @@
 FILE_NAME = "input.txt"
 
 
-def main():
+def get_card_matches(numbers: str) -> int:
+    number_lists = numbers.split(" | ", 1)
+
+    winning_set = set(n for n in number_lists[0].split())
+
+    matches = 0
+    for number in number_lists[1].split():
+        if number in winning_set:
+            matches += 1
+
+    return matches
+
+
+def part1():
     total_points = 0
     with open(FILE_NAME, encoding="utf-8") as f:
         for line in f.readlines():
             card_and_numbers = line.split(": ", 1)
-            number_lists = card_and_numbers[1].split(" | ", 1)
 
-            winning_set = set(n for n in number_lists[0].split())
-
-            winning_numbers_count = 0
-            for number in number_lists[1].split():
-                if number in winning_set:
-                    winning_numbers_count += 1
+            winning_numbers_count = get_card_matches(card_and_numbers[1])
 
             if winning_numbers_count > 0:
                 card_points = 2 ** (winning_numbers_count - 1)
@@ -23,4 +30,4 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    part1()
