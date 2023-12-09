219284360a1679c890bbff32f1e546f8aef88563
Ariel Ribeiro <dsrariel@gmail.com>
2023-12-09 16:23:07 -0300

    answer part2

--- 09/main.py
+++ 09/main.py
@@ -27,9 +27,27 @@ class History:
             new_number = differences[i - 1][-1] + differences[i][-1]
             differences[i - 1].append(new_number)
 
+    def extrapolates_previous_value(self) -> int:
+        differences = self._calculate_differences()
+        previous = 0
+        for i in range(len(differences) - 2, -1, -1):
+            previous = differences[i][0] - previous
+
+        return previous
+
     def extrapolate_next_value(self) -> int:
         self._right_extrapolate()
         return self.numbers[-1]
+
+
+def part2():
+    previous_values = []
+    with open(FILE_NAME, encoding="utf-8") as f:
+        for line in f.readlines():
+            history = History([int(n) for n in line.split(" ")])
+            previous_values.append(history.extrapolates_previous_value())
+
+    print(f"The sum of the extrapolated values is {sum(previous_values)}.")
 
 
 def part1():
@@ -44,3 +62,4 @@ def part1():
 
 if __name__ == "__main__":
     part1()
+    part2()
