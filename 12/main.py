8a3eba74948f7a6eb5df386653c72d938e154f2a
Ariel Ribeiro <dsrariel@gmail.com>
2023-12-16 18:49:28 -0300

    brute force part one

---
+++ 12/main.py
@@ -0,0 +1,77 @@
+from typing import List
+from collections import deque, namedtuple
+
+FILE_NAME = "input.txt"
+
+Row = namedtuple("Row", "arrangement damaged_groups unknown_springs")
+
+
+def is_possible_arrangement(row: Row) -> bool:
+    i, group = 0, 0
+    while i < len(row.arrangement):
+        if row.arrangement[i] == ".":
+            i += 1
+            continue
+
+        if group >= len(row.damaged_groups):
+            return False
+
+        count = 0
+        while i < len(row.arrangement) and row.arrangement[i] == "#":
+            count += 1
+            i += 1
+
+        if count != row.damaged_groups[group]:
+            return False
+
+        group += 1
+
+    return group == len(row.damaged_groups)
+
+
+def get_possible_arrangements_count(row: Row) -> int:
+    if not row.unknown_springs:
+        return int(is_possible_arrangement(row))
+
+    count = 0
+    next_unknown_spring = row.unknown_springs.popleft()
+    row.arrangement[next_unknown_spring] = "."
+    count += get_possible_arrangements_count(row)
+    row.arrangement[next_unknown_spring] = "#"
+    count += get_possible_arrangements_count(row)
+
+    row.arrangement[next_unknown_spring] = "?"
+    row.unknown_springs.appendleft(next_unknown_spring)
+
+    return count
+
+
+def load_input() -> List[Row]:
+    condition_records = []
+
+    with open(FILE_NAME, encoding="utf-8") as f:
+        for line in f.readlines():
+            arrangement_string, damaged_groups_string = line.split(" ", 1)
+
+            arrangement, unknown_springs = [], deque()
+            for i, a in enumerate(arrangement_string):
+                arrangement.append(a)
+                if a == "?":
+                    unknown_springs.append(i)
+
+            row = Row(
+                arrangement, [int(g) for g in damaged_groups_string.split(",")], unknown_springs
+            )
+            condition_records.append(row)
+
+    return condition_records
+
+
+def main():
+    condition_records = load_input()
+    count = sum([get_possible_arrangements_count(r) for r in condition_records])
+    print(f"The sum of possible arrangements is {count}.")
+
+
+if __name__ == "__main__":
+    main()
