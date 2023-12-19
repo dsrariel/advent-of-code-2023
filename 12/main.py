f3019b8823acdc1ade6aa95122c24a0754174976
Ariel Ribeiro <33427789+dsrariel@users.noreply.github.com>
2023-12-18 22:53:49 -0300

    Day 12 (#11)
    * brute force part one
    
    * refactor while
    
    * analyze string before its completely resolved
    
    * rename main as part 1
    
    * unfold input for part two
    
    * add choices to class Row
    
    * add cache to store exploration point
    
    * replace cache by state
    
    * add damaged account optimization
    
    * use multiprocessing
    
    * Revert "use multiprocessing"
    
    This reverts commit f763430f19eec7317c18d705ad80d22717715ac9.
    
    * Revert "add damaged account optimization"
    
    This reverts commit f8915c7aa749aa2c784f7a1f769db5e35172ec51.
    
    * fresh start
    
    * new recursive approach
    
    * add cache
    
    * Row as namedtuple
    
    * add i and group size as arguments
    
    * replace c variable
    
    * parallelize
    
    * retrieve from cache
    
    * remove i and group_size
    
    * solve for input

---
+++ 12/main.py
@@ -0,0 +1,102 @@
+from collections import defaultdict, namedtuple
+from itertools import repeat
+from multiprocessing import Pool, cpu_count
+from typing import Dict, List
+
+FILE_NAME = "input.txt"
+POOL_SIZE = cpu_count() - 2
+
+Row = namedtuple("Row", "arrangement damaged_groups")
+
+
+def get_possible_arrangements_count(row: Row, cache: defaultdict[str, Dict[str, int]]) -> int:
+    arrangement = "".join(row.arrangement)
+    groups = ",".join(map(str, row.damaged_groups))
+
+    if cache.get(arrangement, {}).get(groups, None) is not None:
+        return cache[arrangement][groups]
+
+    if not row.damaged_groups:
+        cache[arrangement][groups] = int(not any(a == "#" for a in row.arrangement))
+        return cache[arrangement][groups]
+
+    group_size = 0
+    for i, c in enumerate(row.arrangement):
+        if c == "?":
+            a = Row(list(row.arrangement), row.damaged_groups)
+            b = Row(list(row.arrangement), row.damaged_groups)
+            a.arrangement[i], b.arrangement[i] = "#", "."
+            cache[arrangement][groups] = get_possible_arrangements_count(
+                a, cache
+            ) + get_possible_arrangements_count(b, cache)
+            return cache[arrangement][groups]
+
+        if c == "#":
+            group_size += 1
+            continue
+
+        if group_size == 0:
+            continue
+
+        if group_size == row.damaged_groups[0]:
+            cache[arrangement][groups] = get_possible_arrangements_count(
+                Row(row.arrangement[i:], row.damaged_groups[1:]),
+                cache,
+            )
+            return cache[arrangement][groups]
+
+        cache[arrangement][groups] = 0
+        return cache[arrangement][groups]
+
+    cache[arrangement][groups] = int(
+        len(row.damaged_groups) == 1 and group_size == row.damaged_groups[0]
+    )
+    return cache[arrangement][groups]
+
+
+def load_input() -> List[Row]:
+    condition_records = []
+
+    with open(FILE_NAME, encoding="utf-8") as f:
+        for line in f.readlines():
+            arrangement_string, damaged_groups_string = line.split(" ", 1)
+            row = Row(list(arrangement_string), [int(g) for g in damaged_groups_string.split(",")])
+            condition_records.append(row)
+
+    return condition_records
+
+
+def unfold_input(condition_records: List[Row]) -> List[Row]:
+    rows = []
+    for row in condition_records:
+        new_arrangement, new_damaged_groups = list(row.arrangement), list(row.damaged_groups)
+        for _ in range(4):
+            new_arrangement.extend("?")
+            new_arrangement.extend(row.arrangement)
+            new_damaged_groups.extend(row.damaged_groups)
+        rows.append(Row(new_arrangement, new_damaged_groups))
+
+    return rows
+
+
+def part_one():
+    condition_records = load_input()
+    count = sum(get_possible_arrangements_count(r, defaultdict(dict)) for r in condition_records)
+    print(f"The sum of possible arrangements is {count}.")
+
+
+def part_two():
+    condition_records = load_input()
+    condition_records = unfold_input(condition_records)
+    with Pool(POOL_SIZE) as p:
+        count = sum(
+            p.starmap(
+                get_possible_arrangements_count, zip(condition_records, repeat(defaultdict(dict)))
+            )
+        )
+    print(f"The sum of possible arrangements is {count}.")
+
+
+if __name__ == "__main__":
+    part_one()
+    part_two()
